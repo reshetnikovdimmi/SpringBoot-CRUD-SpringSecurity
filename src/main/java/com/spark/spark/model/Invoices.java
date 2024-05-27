7730de374e4e737b465e952933cf781296b28527
Дмитрий Решетников <reshetnikov.dim.mi@yandex.ru>
2024-05-27 10:09:33 +0600

    Invoices

--- src/main/java/com/spark/spark/model/Invoices.java
+++ src/main/java/com/spark/spark/model/Invoices.java
@@ -19,7 +19,7 @@ import java.util.Date;
 @ToString
 public class Invoices {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nomenclature;
--- src/main/java/com/spark/spark/model/SalesReport.java
+++ src/main/java/com/spark/spark/model/SalesReport.java
@@ -18,7 +18,7 @@ import java.util.Date;
 @ToString
 public class  SalesReport {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String characteristic;
     private String shop;
